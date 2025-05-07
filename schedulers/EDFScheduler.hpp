1eecd4b3fec949d0694b9755f37b23f25612d68d
Ludvig Sandh <givdul11@hotmail.se>
2025-05-07 15:29:58 +0200

    Preemptive EDF scheduling complete

--- schedulers/EDFScheduler.hpp
+++ schedulers/EDFScheduler.hpp
@@ -5,4 +5,7 @@
 class EDFScheduler : public SchedulingAlgorithm {
 public:
     Schedule GenerateSchedule(TaskSet &taskSet, bool allowPreemptions) override;
+private:
+    Schedule HelpGenerateScheduleWithoutPreemptions(TaskSet &taskSet);
+    Schedule HelpGenerateScheduleWithPreemptions(TaskSet &taskSet);
 };
