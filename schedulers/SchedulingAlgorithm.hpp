a9dd6b96c3ff3f69169ee7dc94f3c5d01f45c193
Ludvig Sandh <givdul11@hotmail.se>
2025-05-08 20:07:49 +0200

    Fixed bug where scheduling stops releasing tasks because hyperperiod is finished

--- schedulers/SchedulingAlgorithm.hpp
+++ schedulers/SchedulingAlgorithm.hpp
@@ -29,7 +29,7 @@ protected:
         TaskJob *runningJob = nullptr;
     
         // Main simulation loop
-        while (currentTime < hyperPeriod) {
+        while (pendingJobs.size() || readyQueue.size()) {
             // Add any tasks that have arrived up until now to the ready queue.
             while (pendingJobs.size() && pendingJobs.front().arrival <= currentTime) {
                 readyQueue.push(pendingJobs.front());
